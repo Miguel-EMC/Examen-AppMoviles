b876485ef8171542454e356c7aa77211ce73f38f
Miguel-EMC <eduardomuzo123456@gmail.com>
2022-11-14 19:22:43 -0500

    Update respect the gallery and photo

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
-import { PhotoService } from './../services/photo.service';
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { UserPhoto, PhotoService } from '../services/photo.service';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -7,10 +9,31 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor(public photoServices: PhotoService) {}
+  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) {}
 
-  addPhotoToGallery(){
-    this.photoServices.addNewToGallery();
+  async ngOnInit() {
+    await this.photoService.loadSaved();
   }
 
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+         }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
